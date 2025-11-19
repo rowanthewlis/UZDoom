c26ba360c0c2579fb60261b0d4e475b147a54ffc
rowanthewlis <35423132+rowanthewlis@users.noreply.github.com>
2025-11-19 11:43:10 +0000

    Update a_dynlight.cpp

--- src/playsim/a_dynlight.cpp
+++ src/playsim/a_dynlight.cpp
@@ -97,6 +97,7 @@ static FDynamicLight *GetLight(FLevelLocals *Level)
 	ret->mShadowmapIndex = 1024;
 	ret->Level = Level;
 	ret->Pos.X = -10000000;	// not a valid coordinate.
+	ret->m_interpolate = true;
 	return ret;
 }
 
